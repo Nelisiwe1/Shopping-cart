aeaed3c5b989179737a021dd548749fa419b1e31
Nelisiwe <nelisiwengqeme3@gmail.com>
2024-08-05 15:48:17 +0200

    6th

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Navbar from './Components/Navbar'
-import Products from './Pages/Products'
-import Footer from './Components/Footer'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Components/Navbar';
+import Products from './Pages/Products';
+import Footer from './Components/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Product from './Components/Product';
+import Cart from './Components/ '; // Ensure this path is correct
 
 function App() {
-
-
   return (
     <BrowserRouter>
       <Navbar />
-    <Routes>
-      <Route path="/" element={<Products></Products>}></Route>
-      <Route path="/cart" element={<cart></cart>}></Route>
-    </Routes>
-    
-      < Footer />
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
+
